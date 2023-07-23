4cab6bad882668c7e6a486174c73169b8f9c4db6
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-07-23 11:18:11 +0330

    02_travel_list: add the ability to create a new item and add it to the list of items.

--- 02_travel_list/src/App.tsx
+++ 02_travel_list/src/App.tsx
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import Logo from "./components/Logo";
 import PackingList from "./components/PackingList";
 import Stats from "./components/Stats";
-import initialItems from "./data";
+import { Item } from "./data";
 
 function App() {
+  const [items, setItems] = useState<Item[]>([]);
+
+  const handleAddItem = (item: Item) => {
+    setItems((prev) => [...prev, item]);
+  };
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList items={initialItems} />
+      <Form onAddItem={handleAddItem} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
--- 02_travel_list/src/components/Form/index.tsx
+++ 02_travel_list/src/components/Form/index.tsx
@@ -1,8 +1,50 @@
-function Form() {
+import { useState } from "react";
+import { Item } from "../../data";
+
+interface FormProps {
+  onAddItem: (item: Item) => void;
+}
+
+function Form({ onAddItem }: FormProps) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!description) {
+      return;
+    }
+    onAddItem({
+      id: Date.now(),
+      quantity,
+      description,
+      packed: false,
+    });
+    setDescription("");
+    setQuantity(1);
+  };
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(parseInt(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((v) => (
+          <option value={v} key={v}>
+            {v}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
