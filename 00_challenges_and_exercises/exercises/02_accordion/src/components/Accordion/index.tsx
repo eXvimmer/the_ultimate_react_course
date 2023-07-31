be7ebe9a37b08f5b3588f2e8c79870af2fefcae8
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-07-31 15:44:06 +0330

    exercise: 02_accordion: make only one accordion item open each time.

--- 00_challenges_and_exercises/exercises/02_accordion/src/components/Accordion/index.tsx
+++ 00_challenges_and_exercises/exercises/02_accordion/src/components/Accordion/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Item, { Question } from "../Item";
 
 interface AccordionProps {
@@ -5,10 +6,24 @@ interface AccordionProps {
 }
 
 function Accordion({ data: questions }: AccordionProps) {
+  const [curOpen, setCurOpen] = useState<number | null>(null);
+
+  const handleOpen = (num: number | null) => {
+    setCurOpen(num);
+  };
+
   return (
     <div className="accordion content-box">
       {questions.map((q, i) => {
-        return <Item number={i + 1} {...q} key={q.title} />;
+        return (
+          <Item
+            {...q}
+            num={i}
+            key={q.title}
+            isOpen={curOpen === i}
+            onOpen={handleOpen}
+          />
+        );
       })}
     </div>
   );
--- 00_challenges_and_exercises/exercises/02_accordion/src/components/Item/index.tsx
+++ 00_challenges_and_exercises/exercises/02_accordion/src/components/Item/index.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
-
 export interface Question {
   title: string;
   text: string;
 }
 
 interface ItemProps extends Question {
-  number: number;
+  num: number;
+  isOpen: boolean;
+  onOpen(num: number | null): void;
 }
 
-function Item({ title, text, number }: ItemProps) {
-  const [isOpen, setIsOpen] = useState(false);
-
+function Item({ title, text, num, isOpen, onOpen }: ItemProps) {
   return (
     <div
       className={`item ${isOpen ? "open" : ""}`}
-      onClick={() => setIsOpen(!isOpen)}
+      onClick={() => {
+        onOpen(isOpen ? null : num);
+      }}
     >
-      <p className="number">{number.toString().padStart(2, "0")}</p>
+      <p className="number">{(num + 1).toString().padStart(2, "0")}</p>
       <h2 className="text">{title}</h2>
       <span className="icon">{isOpen ? "-" : "+"}</span>
       {isOpen && <div className="content-box">{text}</div>}
