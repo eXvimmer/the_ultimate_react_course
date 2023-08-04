17578eff4fead5c90e9413d06dd702a349e1fea5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-08-04 17:53:04 +0330

    03_eat_n_split: create controlled elements.

--- 03_eat_n_split/src/components/FormSplitBill/index.tsx
+++ 03_eat_n_split/src/components/FormSplitBill/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Button from "../../Button";
 import { FriendType } from "../Friend";
 
 interface FormSplitBillProps {
@@ -5,21 +7,52 @@ interface FormSplitBillProps {
 }
 
 function FormSplitBill({ friend }: FormSplitBillProps) {
+  const [bill, setBill] = useState(0);
+  const [userExpense, setUserExpense] = useState(0);
+  const [whoIsPaying, setWhoIsPaying] = useState("user");
+  const friendsExpense = bill > 0 ? bill - userExpense : 0;
+
   return (
     <form className="form-split-bill">
       <h2>Split a bill with {friend.name}</h2>
       <label htmlFor="bill-value">💰 Bill Value</label>
-      <input type="number" id="bill-value" />
-      <label htmlFor="your-expense">🎅 Your expense</label>
-      <input type="number" id="your-expense" />
+      <input
+        type="number"
+        id="bill-value"
+        value={bill}
+        min={0}
+        onChange={(e) => {
+          setBill(parseFloat(e.target.value));
+        }}
+      />
+      <label htmlFor="user-expense">🎅 Your expense</label>
+      <input
+        type="number"
+        id="user-expense"
+        value={userExpense}
+        onChange={(e) => {
+          const v = parseFloat(e.target.value);
+          setUserExpense(v > bill ? userExpense : v);
+        }}
+      />
       <label htmlFor="friends-expense">👭 {friend.name}'s expense</label>
-      <input type="number" id="friends-expense" disabled />
+      <input
+        type="number"
+        id="friends-expense"
+        disabled
+        value={friendsExpense}
+      />
 
       <label htmlFor="who">🤑 Who is paying the bill?</label>
-      <select id="who">
+      <select
+        id="who"
+        value={whoIsPaying}
+        onChange={(e) => setWhoIsPaying(e.target.value)}
+      >
         <option value="user">You</option>
         <option value="friend">{friend.name}</option>
       </select>
+      <Button>Split bill</Button>
     </form>
   );
 }
