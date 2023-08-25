b8b3e6614b1ed6eb637bf90813c95950181f9f0f
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-08-25 15:05:54 +0330

    04_use_popcorn: autofocus on the search input, and also focus on it when pressing the enter key.

--- 04_use_popcorn/src/components/Search/index.tsx
+++ 04_use_popcorn/src/components/Search/index.tsx
@@ -1,9 +1,34 @@
+import { useEffect, useRef } from "react";
+
 interface SearchProps {
   query: string;
   onSetQuery(arg: string): void;
 }
 
 function Search({ query, onSetQuery }: SearchProps) {
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputEl?.current?.focus();
+  }, []);
+
+  useEffect(() => {
+    function callback(e: KeyboardEvent) {
+      if (document.activeElement === inputEl?.current) return;
+
+      if (e.code === "Enter") {
+        inputEl?.current?.focus();
+        onSetQuery("");
+      }
+    }
+
+    document.addEventListener("keydown", callback);
+
+    return () => {
+      document.removeEventListener("keydown", callback);
+    };
+  }, [onSetQuery]);
+
   return (
     <input
       className="search"
@@ -11,6 +36,7 @@ function Search({ query, onSetQuery }: SearchProps) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => onSetQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
