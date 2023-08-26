06e1facf42f7764bc323e4acd958e668d6539fa5
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-08-26 17:55:14 +0330

    challenge: 06_geo_location_hook: create a custom hook that gets user's location and shows it on an open source map.

---
+++ 00_challenges_and_exercises/challenges/06_geo_location_hook/src/App.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+
+function useGeolocation() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [position, setPosition] = useState({ lat: NaN, lng: NaN });
+  const [error, setError] = useState("");
+
+  function getPosition() {
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return { getPosition, isLoading, position, error };
+}
+
+export default function App() {
+  const [countClicks, setCountClicks] = useState(0);
+
+  const {
+    getPosition,
+    position: { lat, lng },
+    error,
+    isLoading,
+  } = useGeolocation();
+
+  function handleClick() {
+    setCountClicks((count) => count + 1);
+    getPosition();
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && !isNaN(lat) && !isNaN(lng) && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
