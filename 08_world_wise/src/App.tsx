72bbd44db5ae61aad2c1e6e0f368cde90d2a2e39
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-09-03 14:22:09 +0330

    08_world_wise: copy some pages from the starter files and fix things.

--- 08_world_wise/src/App.tsx
+++ 08_world_wise/src/App.tsx
@@ -1,18 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import Nav from "./components/Nav";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 
 function App() {
   return (
     <BrowserRouter>
-      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- 08_world_wise/src/pages/Home/index.tsx
+++ 08_world_wise/src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
+import PageNav from "../../components/PageNav";
+
 function Home() {
   return (
-    <div>
-      <h1>World Wise</h1>
-    </div>
+    <main className={styles.homepage}>
+      <PageNav />
+      <section>
+        <h1>
+          You travel the world.
+          <br />
+          WorldWise keeps track of your adventures.
+        </h1>
+        <h2>
+          A world map that tracks your footsteps into every city you can think
+          of. Never forget your wonderful experiences, and show your friends how
+          you have wandered the world.
+        </h2>
+        <Link className="cta" to="/app">
+          Start Tracking Now
+        </Link>
+      </section>
+    </main>
   );
 }
 
