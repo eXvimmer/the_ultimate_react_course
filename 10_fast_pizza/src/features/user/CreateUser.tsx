2f32605c8409e77f82759c0ceadb22773f14cfdb
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-09-28 15:35:23 +0330

    10_fast_pizza: add some initial styling with tailwind.

--- 10_fast_pizza/src/features/user/CreateUser.tsx
+++ 10_fast_pizza/src/features/user/CreateUser.tsx
@@ -1,24 +1,27 @@
-import { useState } from 'react';
+import { FormEventHandler, useState } from "react";
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
-  function handleSubmit(e) {
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+      <p className="mb-4 text-sm text-stone-600 md:text-base">
+        👋 Welcome! Please start by telling us your name:
+      </p>
 
       <input
         type="text"
         placeholder="Your full name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
+        className="w-72"
       />
 
-      {username !== '' && (
+      {username !== "" && (
         <div>
           <button>Start ordering</button>
         </div>
