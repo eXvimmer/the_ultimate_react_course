67d9ff96d79519fcf8956dd8f3df0c50a026d6cb
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-10-02 11:09:48 +0330

    10_fast_pizza: integrate Geolocation.

--- 10_fast_pizza/src/features/order/CreateOrder.tsx
+++ 10_fast_pizza/src/features/order/CreateOrder.tsx
@@ -24,13 +24,20 @@ function CreateOrder() {
   const navigation = useNavigation();
   const thunkDispatch: ThunkDispatch<RootState, null, AnyAction> =
     useDispatch();
-  const username = useRootSelector((s) => s.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    // error: addressError,
+  } = useRootSelector((s) => s.user);
   const cart = useRootSelector(getCart);
   const formErrors = useActionData() as Record<string, string>;
   const totalCartPrice = useRootSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
   const isSubmitting = navigation.state === "submitting";
+  const isLoadingAddress = addressStatus === "loading";
 
   if (!cart.length) {
     return <EmptyCart />;
@@ -39,11 +46,6 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-
-      {/* TODO: change this */}
-      <button onClick={() => thunkDispatch(fetchAddress())}>
-        Get Position
-      </button>
 
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
@@ -69,16 +71,39 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                Couldn't get your address using GPS. Make sure to fill out this
+                field.
+              </p>
+            )}
           </div>
+
+          {!position && (
+            <span className="absolute right-[3px] top-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  thunkDispatch(fetchAddress());
+                }}
+              >
+                Get Position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -97,7 +122,12 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
+          <input
+            type="hidden"
+            name="position"
+            value={position ? `${position.latitude},${position.longitude}` : ""}
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting
               ? "Placing order...."
               : `Order now from ${formatCurrency(totalPrice)}`}
@@ -117,6 +147,7 @@ export const createOrderAction: ActionFunction = async ({
     customer: string;
     phone: string;
     address: string;
+    position: string;
     priority: string;
     cart: string;
   };
@@ -132,7 +163,7 @@ export const createOrderAction: ActionFunction = async ({
     };
   }
   const res = await createOrder(order);
-  store.dispatch(clearCart()); // WARN: HACK: DON'T overuse this technique.
+  store.dispatch(clearCart()); // HACK: WARNING: DON'T overuse this technique.
   return redirect(`/order/${res.id}`);
 };
 
