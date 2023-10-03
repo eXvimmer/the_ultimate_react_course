0e5b5fe9d592d211404cffd37b2f1d572870eafc
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-10-03 15:13:39 +0330

    11_wild_oasis: Create a heading component that can render different kinds of headings.
    Show how to get props in styled components

--- 11_wild_oasis/src/App.tsx
+++ 11_wild_oasis/src/App.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
 
 // TODO: FIX: background color
 const StyledApp = styled.div`
@@ -13,6 +14,9 @@ function App() {
     <>
       <GlobalStyles />
       <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Welcome</Heading>
+        <Heading as="h3">Want to see more?</Heading>
         <Input placeholder="number of guests" type="number" />
         <Button onClick={() => alert("check in")}>Check in</Button>
       </StyledApp>
