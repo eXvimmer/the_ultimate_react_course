8c0c6ef62719362b48dc4c2a1d4dd2b2f3ba0acc
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-10-04 13:13:44 +0330

    11_wild_oasis: setup pages and routes.

---
+++ 11_wild_oasis/src/hooks/useLocalStorageState.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+
+// TODO: set the type of initialState to the proper type
+export function useLocalStorageState(initialState: unknown, key: string) {
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key],
+  );
+
+  return [value, setValue];
+}
