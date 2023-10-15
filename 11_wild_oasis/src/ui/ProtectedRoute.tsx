0b6b9e69ad01c9774f36bab82f0ddb3fb77d2d59
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-10-15 17:12:57 +0330

    11_wild_oasis: allow user access to protected routes, only if they are authenticated.

---
+++ 11_wild_oasis/src/ui/ProtectedRoute.tsx
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+import { useUser } from "../features/authentication/useUser";
+import { ChildrenProps } from "../types";
+import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+function ProtectedRoute({ children }: ChildrenProps) {
+  const { isLoading, isAuthenticated } = useUser();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated && !isLoading) {
+      navigate("/login");
+    }
+  }, [isAuthenticated, isLoading, navigate]);
+
+  if (isLoading) {
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+  }
+  if (isAuthenticated) {
+    return children;
+  }
+}
+
+export default ProtectedRoute;
